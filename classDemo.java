5f3bb128c53538db4423cc831387d4254a948631
paragkshirsagar01 <paragkshirsagar18@gmail.com>
2023-05-05 20:51:53 +0530

    day 2

--- classDemo.java
+++ classDemo.java
@@ -1,19 +1,35 @@
+
+
 // public private protected default
 class demo{
-	int a=10;
-	int b=20;
+	demo(){
+		System.out.println("default constructor");//default const.
+	}
+	demo(int a){
+		System.out.println("default constructor 2");//parameterized const.
+	}
+	demo(int a,int b){
+		for(int i=0;i<10;i++)
+		System.out.println(this.sum(a,b));
+		if(a==10)
+		System.out.println("matched");
+	}
 	//access return type name(parameters)
-	public int sum(int a,int b){
+	public int sum(int a,int b){//creating methods(functions)
 		//System.out.println(a+b);
 		return(a+b);
 	}
 }
+
+
+
 public class classDemo{
 	public static void main(String[] args){
 		int a=40;
-		demo x=new demo();
+		demo x=new demo(20,60); //creating objects of class demo
 		//System.out.println(x.a);
-		int ans=x.sum(10,(int)20.2);
-		System.out.println(ans);
 	}
 }
+
+
+
