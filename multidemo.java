51f822350522e58cc58943e0e895a73674b5e6a2
paragkshirsagar01 <paragkshirsagar18@gmail.com>
2023-05-19 22:01:38 +0530

    multithreading

---
+++ multidemo.java
@@ -0,0 +1,29 @@
+class printMsg{
+	void print(String msg){
+		for(int i=0;i<11;i++){
+			System.out.println(msg+"  "+i);
+		}
+	}
+}
+class demothread extends Thread{
+	printMsg m;
+	String msg;
+	demothread(printMsg m,String msg){
+		this.m=m;
+		this.msg=msg;
+	}
+	public void run(){
+	m.print(this.msg);
+	}
+}
+public class multidemo{
+	public static void main(String[] args){
+		printMsg m=new printMsg();
+		//m.print("parag");//task1
+		demothread d1=new demothread(m,"parag");
+		demothread d2=new demothread(m,"xyz");
+		d1.start();
+		d2.start();
+		System.out.println("finished");//task2 
+	}
+}
